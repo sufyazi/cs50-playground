eba8f98ee073b803566c32adc12f8c3d8c895e5d
sufyazi <sufyazi@outlook.com>
2024-05-07 19:11:17 +0800

    wrote test scripts

---
+++ mario.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int length); // this is function prototype
+
+int main(void) {
+    int height = get_int("Height: ");
+    for (int i = 0; i < height; i++) {
+        print_row(i + 1);
+    }
+}
+
+void print_row(int length) {
+    for (int i = 0; i < length; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
